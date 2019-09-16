7853c1bbcbc458e213c770d400bcd46550bd786a
Bart <bartstorm1@gmail.com>
2019-09-16 11:45:56 +0200

    Dit is de versie die goed is. het is een website van de pizza calculator.

--- PizzaCalc.js
+++ PizzaCalc.js
@@ -1,7 +1,15 @@
-document.write("Keuze uit 3 soorten pizza's")
-document.write('<br>')
-document.write("Klein" + "  " + "€5,-")
-document.write('<br>')
-document.write("Middel" + "  " + "€7,-")
-document.write('<br>')
-document.write("Groot" + "  " + "€9,-")
+var num1 = prompt("Hoeveel kleine pizza's wilt u? (€5)");
+var num2 = prompt("Hoeveel medium pizza's wilt u? (€7)");
+var num3 = prompt("Hoeveel grote pizza's wilt u? (€9)");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+num3 = parseFloat(num3);
+
+document.write("Het eindbedrag is:" + (num1 * 4 + num2 * 6 + num3 * 9));
+document.write("<br/>");
+document.write("Het aantal kleine pizza's zijn:" + (num1));
+document.write("<br/>");
+document.write("Het aantal medium pizza's zijn:" + (num2));
+document.write("<br/>");
+document.write("Het aantal grote pizza's zijn:" + (num3));
